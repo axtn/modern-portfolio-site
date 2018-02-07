fdc393b1f26df85084660bee6a4dbd3b43cb69f1
Alex Tadevosyan <axtn95@gmail.com>
2018-02-07 02:37:35 -0800

    Email spam protection

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-// console.log("hello");
+// Prevent bots from spamming my email
+
+(function() {
+    var emailButton = document.querySelector('.button--email');
+    var axtnEmail = 'hi@' + 'axtn.io';
+
+    var addEmail = function() {
+        this.href="mailto:" + axtnEmail;
+    }
+
+    emailButton.addEventListener('click', addEmail);
+    emailButton.addEventListener('contextmenu', addEmail);
+})();
